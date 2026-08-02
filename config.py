c1660e20e85dbf188c921d430a56cbc45285d595
Diva Amalia <divaamaliaaljanah@mail.ugm.ac.idgit config --global user.email>
2026-08-02 15:48:39 +0800

    Prepare for first deployment

--- config.py
+++ config.py
@@ -23,9 +23,8 @@ class Config:
         os.path.dirname(__file__)
     )
 
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///"
-        + os.path.join(BASE_DIR, "sunflower.db")
-    )
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL",
+    "sqlite:///" + os.path.join(BASE_DIR, "sunflower.db")
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
